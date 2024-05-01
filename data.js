fe2d444f5c483210716d3c6209c7d59b3cb935b4
Sakshib190 <106507441+Sakshib190@users.noreply.github.com>
2024-05-01 22:30:24 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,31 @@
+
+
+export const navData = [
+    { url: 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/63178e3e64d503a479f2a2048a474552', text: 'Offers' },
+    { url: 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/b1120358822d622eedd1595a9a937119', text: 'Italian' },
+    { url: 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/403038c4f28043ef59b37604223d14e5', text: 'Chinese' },
+    { url: 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/7a86de986752271f1c1b2748d7a4154b', text: 'American' },
+    { url: 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/mbxigw4lllaydezvkdhe', text: 'Indian' },
+    { url: 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/aofpo9vhugnfzobw3972', text: 'Mexican' },
+    { url: 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/wd9qx90stwwkhumhv11r', text: 'Korean' },
+    { url: 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/e2283207c3b419a8fe6460b83f09d361', text: 'Thai' },
+    { url: 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/94427c09ade19e06fb9f84c10786effe', text: 'More' }
+];
+
+export const bannerData = [
+    { id: 1, url: 'https://www.mobikwik.com/blog/wp-content/uploads/2017/05/1140x300-7.jpg' },
+    { id: 2, url: 'https://cdn.kibrispdr.org/data/9/background-banner-food-51.jpg' },
+    { id: 3, url: 'https://as2.ftcdn.net/v2/jpg/03/25/74/41/1000_F_325744171_NPeVOg6T8URi6L7KLnaJRp21HLHmYA7l.jpg' },
+    { id: 4, url: 'https://png.pngtree.com/thumb_back/fh260/background/20190222/ourmid/pngtree-crayfish-food-food-condiment-black-horizontal-banner-goodsseasoningblackhorizontal-bannerdelicious-image_50376.jpg' }
+]
+
+export const imageURL = [
+    'https://media.istockphoto.com/id/1316145932/photo/table-top-view-of-spicy-food.jpg?s=612x612&w=0&k=20&c=eaKRSIAoRGHMibSfahMyQS6iFADyVy1pnPdy1O5rZ98=',
+    'https://www.southernliving.com/thmb/3x3cJaiOvQ8-3YxtMQX0vvh1hQw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/2652401_QFSSL_SupremePizza_00072-d910a935ba7d448e8c7545a963ed7101.jpg',
+    'https://i.pinimg.com/736x/49/26/b7/4926b759651a3e0c15213e4c1cb9fabb.jpg'
+];
+
+
+
+
+
